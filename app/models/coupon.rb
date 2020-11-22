9793c814c557bec8612bd2c4f36be25a485835b6
Nathalia Chaves <nathalia.chaves@briotech.com.br>
2020-11-22 14:42:23 -0300

    Incluir future_validator ao Coupon

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -3,5 +3,6 @@ class Coupon < ApplicationRecord
   validates :code, :status, :due_date, :discount_value, presence: true
   validates :code, uniqueness: { case_sensitive: false }
   validates :discount_value, numericality: { greater_than: 0 }
+  validates :due_date, future_date: true
   enum status: STATUS
 end
