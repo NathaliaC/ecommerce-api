39fd40bc3146f217af07527b5d48160755b37310
Nathalia Chaves <nathalia.chaves@briotech.com.br>
2020-11-22 14:27:33 -0300

    Criar validação para data

--- spec/validators/future_date_validator_spec.rb
+++ spec/validators/future_date_validator_spec.rb
@@ -8,7 +8,7 @@ class Validatable
 end
 
 describe FutureDateValidator do
-  subject { Validator.new }
+  subject { Validatable.new }
 
   context 'when date is before current date' do
     before { subject.date = 1.day.ago }
@@ -19,14 +19,28 @@ describe FutureDateValidator do
 
     it 'adds an error on model' do
       subject.valid?
-
       expect(subject.errors.keys).to include(:date)
     end
   end
 
   context 'when date is iqual current date' do
+    before { subject.date = Time.zone.now }
+
+    it 'should be invalid' do
+      expect(subject.valid?).to be_falsey
+    end
+
+    it 'adds an error on model' do
+      subject.valid?
+      expect(subject.errors.keys).to include(:date)
+    end
   end
 
   context 'when date is greater than current date' do
+    before { subject.date = Time.zone.now + 1.day }
+
+    it 'should be valid' do
+      expect(subject.valid?).to be_truthy
+    end
   end
 end
