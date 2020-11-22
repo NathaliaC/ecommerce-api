c943a857fdbe78d9f199e66d61e60db440303d1f
Nathalia Chaves <nathalia.chaves@briotech.com.br>
2020-11-21 23:45:58 -0300

    Criar Model Product + Testes de Modelo

--- app/models/system_requirement.rb
+++ app/models/system_requirement.rb
@@ -1,4 +1,5 @@
 class SystemRequirement < ApplicationRecord
+  has_many :games, dependent: :restrict_with_error
   validates :name, :operational_system, :storage, :processor, :memory, :video_board, presence: true
   validates :name, uniqueness: { case_sensitive: false }
 end
