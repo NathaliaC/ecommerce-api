9a45c9d96310b5adbfdb70a793599c29a851014b
Nathalia Chaves <nathalia.chaves@briotech.com.br>
2020-11-22 18:20:25 -0300

    criar método destroy para categoria

--- app/controllers/admin/v1/categories_controller.rb
+++ app/controllers/admin/v1/categories_controller.rb
@@ -1,5 +1,7 @@
 module Admin::V1
   class CategoriesController < ApiController
+    before_action :load_category, only: %i[update destroy]
+
     def index
       @categories = Category.all
     end
@@ -11,9 +13,14 @@ module Admin::V1
     end
 
     def update
-      @category = Category.find(params[:id])
       @category.attributes = category_params
       save_category!
+    end
+
+    def destroy
+      @category.destroy
+    rescue
+      render_error(fields: @category.errors.messages)
     end
 
     private
@@ -29,6 +36,10 @@ module Admin::V1
       render :show
     rescue
       render_error(fields: @category.errors.messages)
+    end
+
+    def load_category
+      @category = Category.find(params[:id])
     end
   end
 end
