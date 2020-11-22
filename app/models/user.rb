b0f2bea5fddc9329d213483b675ebf61ae943c12
Nathalia Chaves <nathalia.chaves@briotech.com.br>
2020-11-22 00:14:39 -0300

    Refatorar Model User + Criação de Testes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
-# frozen_string_literal: true
-
 class User < ActiveRecord::Base
+  PROFILES = { admin: 0, client: 1 }.freeze
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
+
+  validates :name, :profile, presence: true
+  enum profile: PROFILES
 end
