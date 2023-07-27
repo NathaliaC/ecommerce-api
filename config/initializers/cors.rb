3f0d5a471a98e62b2be16e436f9b2ef88aeb8c9a
Nathalia Chaves <nathalia.chaves2@gmail.com>
2023-07-27 14:54:46 -0300

    cors expose update

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,7 +5,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins '*'
 
     resource '*',
-             headers: :any,
-             methods: %i[get post put patch delete options head]
+      headers: :any,
+      methods: %i[get post put patch delete options head],
+      expose: ['access-token', 'client', 'expiry', 'token-type', 'uid']
   end
 end
